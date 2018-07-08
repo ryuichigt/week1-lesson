58de3bdb529e330fc75cee1d0f535c8656c4f3ac
maro <amoeba3215da@gmail.com>
2018-07-08 16:22:19 +0900

    授業課題

--- C029.py
+++ C029.py
@@ -3,27 +3,27 @@
 # Let's チャレンジ！！
 
 vacation = input().split()
-vacation = list(map(int,vacation))
+vacation = list(map(int, vacation))
 
 rainy_percent = []
 day = []
 
 for i in range(vacation[0]):
     rainy = input().split()
-    rainy = list(map(int,rainy))
+    rainy = list(map(int, rainy))
     rainy_percent.append(rainy[1])
     day.append(rainy[0])
 
-#rainy_percent rpの略
+# rainy_percent rpの略
 
-rp_sum = 100*vacation[1]
+rp_sum = 101*vacation[1]
 
 days = []
 
-for i in range(vacation[0]- vacation[1] + 1):
+for i in range(vacation[0] - vacation[1] + 1):
     if rp_sum > sum(rainy_percent[i:i+vacation[1]]):
         rp_sum = sum(rainy_percent[i:i+vacation[1]])
-        days = [day[i],day[i+vacation[1]-1]]
+        days = [day[i], day[i+vacation[1]-1]]
 
-days = list(map(str,days))   
+days = list(map(str, days))
 print(" ".join(days))
